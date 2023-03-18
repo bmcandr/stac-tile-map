3643109f8a4e91942f2b83018fd74b81e65548b6
Brendan McAndrew <19274445+bmcandr@users.noreply.github.com>
2023-03-18 17:11:19 -0400

    Use Fields in schema, update default to Natural Earth populated places

--- src/python/schemas.py
+++ src/python/schemas.py
@@ -1,9 +1,23 @@
-from pydantic import AnyHttpUrl, BaseModel
+from pydantic import AnyHttpUrl, BaseModel, Field
 
 
 class Inputs(BaseModel):
-    geojson_path: str = "data/national-parks.geojson"
-    catalog: AnyHttpUrl = "https://earth-search.aws.element84.com/v1"
-    collection: str = "sentinel-2-l2a"
-    asset_key: str = "visual"
-    search_period: int = 30
+    geojson_path: str = Field(
+        description="A URL to a GeoJSON file",
+        default="https://raw.githubusercontent.com/martynafford/natural-earth-geojson/master/110m/cultural/ne_110m_populated_places.json",
+    )
+    catalog: AnyHttpUrl = Field(
+        description="A URL to a STAC catalog",
+        default="https://earth-search.aws.element84.com/v1",
+    )
+    collection: str = Field(
+        description="The name of the STAC collection to search",
+        default="sentinel-2-l2a",
+    )
+    asset_key: str = Field(
+        description="The asset key of the COG to display", default="visual"
+    )
+    search_period: int = Field(
+        description="The period (in days) to search backwards starting from today",
+        default=30,
+    )
